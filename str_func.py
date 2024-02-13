91a8d68178864eea34e99587aecefee808bd1571
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-02-14 00:34:34 +0300

    Add capitalize function

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def upper_case(string):
     Make upper-case letters
     """
     return string.upper()
+
+def capitalize_func(string):
+    """
+    Capitalize sentence
+    """
+    return string.capitalize()
